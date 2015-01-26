8ea25fde77e4d43181e8d1a8972faf45bd009599
Kay Lai <clearsky0305@gmail.com>
2015-01-26 19:08:25 +0800

    1. Add cmx api.

--- routes/cmx.js
+++ routes/cmx.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 var config = require('config');
 var cmxConfig = config.get('cmx');
+var debug = require('debug')('cmx');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource' + JSON.stringify(cmxConfig));
+  res.send(cmxConfig.validator);
+});
+
+router.post('/', function(req, res) {
+  debug(req.body);
 });
 
 module.exports = router;
